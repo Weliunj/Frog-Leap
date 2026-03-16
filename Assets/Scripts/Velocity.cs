2967f076ae96e2a90b6c957a40301d198a98b6bf
Quy <anhduyhh123@gmail.com>
2026-03-16 20:04:50 +0700

    14/03 First commit

---
+++ Assets/Scripts/Velocity.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Velocity : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 startTouch = new Vector2(0, 0);
+        Input.Get
+    }
+}
